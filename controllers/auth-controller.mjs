209900a826ab6556d31de9e82acf50e29a2b2c46
N1N3T4L3S <6765614+ninetales@users.noreply.github.com>
2024-06-29 17:42:58 +0200

    :zap: feat: added register endpoint added error response model

--- controllers/auth-controller.mjs
+++ controllers/auth-controller.mjs
@@ -1,4 +1,5 @@
 import User from '../models/user-model.mjs';
+import ErrorResponse from '../models/ErrorResponseModel.mjs';
 import { asyncHandler } from '../middleware/async-handler.mjs';
 
 /**
@@ -12,6 +13,38 @@ export const register = asyncHandler(async (req, res, next) => {
     sendAuthToken(user, 201, res);
 });
 
+/**
+ * @desc   Login a user
+ * @route  POST /api/v1/auth/login
+ * @access Public
+ */
+export const login = asyncHandler(async (req, res, next) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return next(new ErrorResponse('Please provide an email and password', 400));
+    }
+
+    const user = await User.findOne({ email }).select('+password');
+    if (!user) {
+        return next(new ErrorResponse('Invalid credentials', 401));
+    }
+
+    const isValidPassword = await user.validatePassword(password);
+    if (!isValidPassword) {
+        return next(new ErrorResponse('Invalid credentials', 401));
+    }
+
+    sendAuthToken(user, 200, res);
+
+});
+
+/**
+ * @desc Generates a token and sends it in the response
+ * @param {Object} user - contains the user object data
+ * @param {Number} statusCode - contains the HTTP status code to be sent in the response
+ * @param {Object} res - contains the express response object
+ */
 const sendAuthToken = (user, statusCode, res) => {
     const token = user.generateAuthToken();
     res.status(200).json({ sucess: true, statusCode, token });
--- routes/auth-routes.mjs
+++ routes/auth-routes.mjs
@@ -1,12 +1,12 @@
 import express from 'express';
-import { register } from '../controllers/auth-controller.mjs';
+import {
+    register,
+    login
+} from '../controllers/auth-controller.mjs';
 
 const router = express.Router();
 
-// router.get('/', (req, res, next) => {
-//     res.status(200).json({ success: true, message: 'Auth route' });
-// });
-
 router.post('/register', register);
+router.post('/login', login);
 
 export default router;
